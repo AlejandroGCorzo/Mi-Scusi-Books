94f83475c7411d8bdb1777beaa05bf87200df2d5
Juanfrancoledesma <juanfledesma18@gmail.com>
2022-10-10 15:56:00 -0300

    model user

--- api/src/models/user.js
+++ api/src/models/user.js
@@ -1,17 +1,56 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: Number,
-    required: true,
-  },
-  email: {
-    tpye: String,
-  },
-});
+const userSchema = mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    dni: {
+      type: Number,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      street: String,
+      number: Number,
+      floor: Number,
+    },
+    birthdate: {
+      type: Date,
+      required: true,
+    },
+    loyaltyPoint: {
+      type: Number,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    bills: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Bills",
+    },
+  }, { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
