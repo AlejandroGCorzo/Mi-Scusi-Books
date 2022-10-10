0bd8bb26953c13afaf44c219fa31a49060978b77
AlejandroGCorzo <alejandro.g.corzo@gmail.com>
2022-10-10 14:58:27 -0300

    inicializado Details y generado el routing

---
+++ client/src/components/Details.jsx
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+export default function Details() {
+  return <div></div>;
+}
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getChars } from '../redux/booksActions.js';
+import { Link } from 'react-router-dom';
 
-const Home = () => {
+export default function Home() {
   const dispatch = useDispatch();
   const { books } = useSelector((state) => state.books);
   useEffect(() => {
@@ -12,13 +13,13 @@ const Home = () => {
   return (
     <div>
       {books?.map((el) => (
-        <div key={el.ISBN}>
-          <img src={el.image} />
-          <span>{el.name}</span>
+        <div key={el._id}>
+          <Link to={`/books/${el._id}`}>
+            <img src={el.image} />
+            <span>{el.name}</span>
+          </Link>
         </div>
       ))}
     </div>
   );
-};
-
-export default Home;
+}
