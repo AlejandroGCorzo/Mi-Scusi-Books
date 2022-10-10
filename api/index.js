ab87f6ad7f4e57c36e359e87163dde817cbc1c61
unknown <ricaudjuan@hotmail.com>
2022-10-10 12:45:39 -0300

    Revert "added api folder and base db conn"
    This reverts commit 730f76dff5b65d6ced3ad0938d3e215e8be7df27.

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-
-
-conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log('%s listening at 3001');
-  });
-});
