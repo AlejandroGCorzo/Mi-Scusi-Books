3006df50502ff9ab1d44a12c39f1f3320166c072
AlejandroGCorzo <alejandro.g.corzo@gmail.com>
2022-10-11 18:51:30 -0300

    cambio prueba

--- pruebaConflictos.js
+++ pruebaConflictos.js
@@ -1,3 +1,15 @@
 console.log('esta es la prueba del invierno, tuturrito');
 
 console.log("prueba jano")
+console.log('prueba scusi');
+
+let json = `
+{
+    "Arte, Arquitectura y Diseño": {
+        "Historia Del Arte": {
+
+        }
+    }
+}`;
+
+console.log(JSON.parse(json));
