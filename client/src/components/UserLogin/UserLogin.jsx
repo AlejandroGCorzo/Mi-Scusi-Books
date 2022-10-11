9b7b54108f9c0965186a6156726b66dfd0294a79
cassirambd <cassiram15@gmail.com>
2022-10-11 19:15:58 -0300

    cambio en history

--- client/src/components/UserLogin/UserLogin.jsx
+++ client/src/components/UserLogin/UserLogin.jsx
@@ -32,11 +32,12 @@ export default function UserLogin() {
   };
 
   const handleSubmit = (e) => {
+    e.preventDefault();
     setInput({
       ...input,
       [e.target.name]: e.target.value,
     });
-    history.push('/home')
+    history.push("/");
   };
 
   function validate(input) {
@@ -44,13 +45,17 @@ export default function UserLogin() {
 
     if (!input.email) {
       errors.email = "Email is required";
-    } else if (!/\S+@\S+\.\S+/.test(input.email)) {
+    } else if (!/^\S+@\S+\.\S+$/.test(input.email)) {
       errors.email = "Email is invalid";
     }
 
     if (!input.password) {
       errors.password = "Password is required";
-    } else if (!/^(?=(.*[0-9]))(?=.*[a-z])(?=(.*[A-Z]))(?=.*\d)(?=(.*)).{8,15}$/.test(input.password)) {
+    } else if (
+      !/^(?=(.*[0-9]))(?=.*[a-z])(?=(.*[A-Z]))(?=.*\d)(?=(.*)).{8,15}$/.test(
+        input.password
+      )
+    ) {
       errors.password = "Password is invalid ";
     }
     return errors;
@@ -58,55 +63,65 @@ export default function UserLogin() {
   //verificar existencia
 
   return (
-    <>
-      <div>
-        <img
-          src="http://cdn.onlinewebfonts.com/svg/img_568656.png"
-          width="110px"
-          height="110px"
-        />
-        <h1>LOGIN</h1>
-      </div>
-      <form onSubmit={handleSubmit}>
+    <div>
+      <section>
         <div>
-          <label>Email: </label>
-          <input
-            type="text"
-            name="email"
-            placeholder="you@example.com"
-            value={input.email}
-            onChange={handleInputChange}
-            required
+          <img
+            src="http://cdn.onlinewebfonts.com/svg/img_568656.png"
+            width="110px"
+            height="110px"
           />
-          <span>
-            {errors.email && <p>{errors.email}</p>}
-          </span>
+          <h1>LOGIN</h1>
         </div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Email: </label>
+            <input
+              type="text"
+              name="email"
+              placeholder="you@example.com"
+              value={input.email}
+              onChange={handleInputChange}
+              required
+            />
+            <span>{errors.email && <p>{errors.email}</p>}</span>
+          </div>
+          <div>
+            <label>Password: </label>
+            <input
+              type="password"
+              name="password"
+              minLength={8}
+              maxLength={15}
+              placeholder="123456Aa"
+              value={input.password}
+              onChange={handleInputChange}
+              required
+            />
+            <span>{errors.password && <p>{errors.password}</p>}</span>
+          </div>
+          <div>
+            <label>
+              <input type="checkbox" id="cb1" value="cb" />
+              Remember me
+            </label>
+
+            <Link
+              to={"/login/password_reset"}
+              style={{ textDecoration: "none" }}
+            >
+              <span>Forgot password?</span>
+            </Link>
+          </div>
+          <button type="submit">SIGN IN</button>
+        </form>
         <div>
-          <label>Password: </label>
-          <input
-            type="password"
-            name="password"
-            minLength={8}
-            maxLength={15}
-            placeholder="123456Aa"
-            value={input.password}
-            onChange={handleInputChange}
-            required
-          />
-          <span>
-            {errors.password && <p>{errors.password}</p>}
-          </span>
-          <p>Forgot password?</p>
+          <h3>Don't have an account?</h3>
+          <Link to={"/signup"}>
+            <button>SIGN UP</button>
+          </Link>
         </div>
-        <button type="submit">SIGN IN</button>
-      </form>
-      <div>
-        <h3>Don't have an account?</h3>
-        <Link to={"/signup"}>
-          <button>SIGN UP</button>
-        </Link>
-      </div>
-    </>
+      </section>
+    </div>
   );
 }
