94ee8de490f5e905722d97b23cdd756622ece36a
JanoNanzer <janonanzer@gmail.com>
2022-10-13 10:33:33 -0300

    FRONT userActions use endPoint backend && HeaderNav Login bug fix

--- client/src/redux/StoreUsers/usersActions.js
+++ client/src/redux/StoreUsers/usersActions.js
@@ -1,42 +1,11 @@
 import axios from "axios";
 import { getAllUsers, getLoggedUserData } from "./usersSlice.js";
 
-// import { useAuth0 } from "@auth0/auth0-react";
-// const { getAccessTokenSilently } = useAuth0();
-
-export const getUser = () => (dispatch) => {
-  dispatch(
-    getAllUsers([
-      {
-        email: "juanfledesma18@gmail.com",
-        password: 12345,
-      },
-      {
-        email: "janonanzer@gmail.com",
-        password: 12345,
-      },
-      {
-        email: "alejandrogcorzo@gmail.com",
-        password: 12345,
-      },
-      {
-        email: "cassiram15@gmail.com",
-        password: 12345,
-      },
-      {
-        email: "mgutierrezxred@gmail.com",
-        password: 12345,
-      },
-      {
-        email: "agustinnicolas12340@gmail.com",
-        password: 12345,
-      },
-      {
-        email: "ricaudjuan11@gmail.com",
-        password: 12345,
-      },
-    ])
-  );
+export const getUser = () => {
+  return async (dispatch) => {
+    let json = await axios.get('http://localhost:9000/user')
+    return dispatch(getAllUsers(json.data))
+  }
 };
 
 export const getUserDetail = (data) => {
