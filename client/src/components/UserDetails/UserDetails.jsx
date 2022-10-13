6dffb08e8f1566f1b46bf49fc0662fb5e74ad711
JanoNanzer <janonanzer@gmail.com>
2022-10-13 17:40:27 -0300

    FRONT add UserInfo css

--- client/src/components/UserDetails/UserDetails.jsx
+++ client/src/components/UserDetails/UserDetails.jsx
@@ -11,23 +11,26 @@ const UserDetails = () => {
   return (
     <div className="userOuterDiv">
       <div className="userInnerDiv">
-        <h1>Profile ✎</h1>
+        <div className="titlePencilContainer">
+          <h1 style={{margin:'0'}}>Profile</h1>
+          {/* <p className="pencilIcon">✎</p> */}
+        </div>
         <img src={loggedUser.picture} className="userImage" />
-        <span>Username: {loggedUser.nickname}</span>
-        <span>First Name: {loggedUser.firstName}</span>
-        <span>Last Name: {loggedUser.lastName}</span>
-        <span>E-mail: {loggedUser.email}</span>
-        <span>DNI: {loggedUser.dni}</span>
-        <span>Phone: {loggedUser.phone}</span>
-
-        <span>Address: </span>
-        <span>Street: {loggedUser.address.street}</span>
-        <span>Number: {loggedUser.address.number}</span>
-        <span>Floor: {loggedUser.address.floor}</span>
-
-        <span>Birthday: {loggedUser.birthdate}</span>
-        <span>Verify: {loggedUser.state}</span>
-        <span>Bills: {loggedUser.bills}</span>
+        <div className="userInfoContainer">
+          <span>Username: {loggedUser.nickname}</span>
+          <span>First Name: {loggedUser.firstName}</span>
+          <span>Last Name: {loggedUser.lastName}</span>
+          <span>E-mail: {loggedUser.email}</span>
+          <span>DNI: {loggedUser.dni}</span>
+          <span>Phone: {loggedUser.phone}</span>
+          <span>Address: </span>
+          <span>Street: {loggedUser.address.street}</span>
+          <span>Number: {loggedUser.address.number}</span>
+          <span>Floor: {loggedUser.address.floor}</span>
+          <span>Birthday: {loggedUser.birthdate}</span>
+          <span>Verify: {loggedUser.state}</span>
+          <span>Bills: {loggedUser.bills}</span>
+        </div>
       </div>
     </div>
   );
