daded117e11fb8861d593f7b2c07f7ccb88a2d14
Juanfrancoledesma <juanfledesma18@gmail.com>
2022-10-13 11:17:02 -0300

    update get user

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -2,56 +2,76 @@ const { Router } = require("express");
 const User = require("../models/user");
 const axios = require("axios");
 
-
 const userRouter = Router();
 
 //create user
+// {
+//   sub: 'auth0|63474946bce9a900112d95f3',
+//   nickname: 'nanzerjano',
+//   name: 'nanzerjano@hotmail.com',
+//   picture: 'https://s.gravatar.com/avatar/3caa95d2ff0861b0f2235c0e3ff9deb4?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fna.png',
+//   updated_at: '2022-10-13T12:57:47.498Z',
+//   email: 'nanzerjano@hotmail.com'
+// }
 
-userRouter.get("/protected", async (req,res) => {
-  try{
-    const accesToken = req.headers.authorization.split(' ')[1]
-    console.log(accesToken)
-    const response = await axios.get('https://miscusibooks.us.auth0.com/userinfo', {
-      headers: {
-        authorization: `Bearer ${accesToken}`
-      }
-    })
-    const userInfo = response.data
-    console.log(userInfo);
-    res.send(userInfo)
-  } catch(e){
-    res.send(e.message)
-  }
-})
 
-userRouter.post("/", async (req, res) => {
+userRouter.get("/detail", async (req, res) => {
   try {
-    const {
-      userName,
-      password,
-      firstName,
-      lastName,
-      email
-    } = req.body;
-
-    if (
-      !userName ||
-      !password ||
-      !firstName ||
-      !lastName ||
-      !email 
-    )
-      return res.status(400).send("necessary data missing!");
-
-    const repeatedUsername = await User.findOne({ userName: userName });
-    if (repeatedUsername) return res.status(400).send("Username alredy exist!");
-    
-    const newUser = await User.create(req.body);
-    res.send("User created successfully!");
+    const accesToken = req.headers.authorization.split(" ")[1];
+    const response = await axios.get(
+      "https://miscusibooks.us.auth0.com/userinfo",
+      {
+        headers: {
+          authorization: `Bearer ${accesToken}`,
+        },
+      }
+    );
+    const userInfo = response.data;
+    console.log(userInfo);
+    const user = await User.findOne({email : userInfo.email})
+    // console.log(user);
+    const formatUser = {
+      id : user._id,
+      sub : userInfo.sub,
+      email : user.email,
+      picture : userInfo.picture,
+      nickname: userInfo.nickname,
+      userName : user.userName,
+      firstName : user.firstName,
+      lastName : user.lastName,
+      dni : user.dni,
+      phone : user.phone,
+      address: user.address,
+      birthdate : user.birthdate,
+      type: user.type,
+      bills : user.bills? user.bills : 'empty',
+      state: user.state,
+    }
+    // console.log(formatUser);
+    res.send(formatUser);
   } catch (e) {
-    res.status(400).send({ error: e });
+    res.send(e.message);
   }
 });
+
+// userRouter.post("/", async (req, res) => {
+//   try {
+//     const { userName, password, firstName, lastName, email } = req.body;
+
+//     if (!userName || !password || !firstName || !lastName || !email)
+//       return res.status(400).send("necessary data missing!");
+
+//     const repeatedUsername = await User.findOne({ userName: userName });
+//     if (repeatedUsername) return res.status(400).send("Username alredy exist!");
+
+//     const newUser = await User.create(req.body);
+//     res.send("User created successfully!");
+//   } catch (e) {
+//     res.status(400).send({ error: e });
+//   }
+// });
+
+// userRouter.get('/:email', async (req, res) => {})
 
 userRouter.get("/", async (req, res) => {
   try {
@@ -80,7 +100,7 @@ userRouter.delete("/:id", async (req, res) => {
   try {
     const userDeleted = await User.deleteOne({ _id: id });
     if (!userDeleted.deletedCount) return res.send("User not found!");
-    res.send({msg:"User deleted successfully!"});
+    res.send({ msg: "User deleted successfully!" });
   } catch (e) {
     res.status(400).send({ msg: e });
   }
@@ -90,9 +110,12 @@ userRouter.put("/delete/:id", async (req, res) => {
   const { id } = req.params;
   if (!id) return res.status(400).send({ msg: "Not id found!" });
   try {
-    const deletedUser = await User.updateOne({ _id: id }, { $set: {state : "Inactive"} });
-    if(!deletedUser.matchedCount) return res.send("User not found!");
-    res.send({msg:"User deleted successfully!"});
+    const deletedUser = await User.updateOne(
+      { _id: id },
+      { $set: { state: "Inactive" } }
+    );
+    if (!deletedUser.matchedCount) return res.send("User not found!");
+    res.send({ msg: "User deleted successfully!" });
   } catch (e) {
     res.status(400).send({ msg: e });
   }
@@ -104,13 +127,10 @@ userRouter.put("/update/:id", async (req, res) => {
   if (!id) return res.status(400).send({ msg: "Not id found!" });
   try {
     const updatedUser = await User.updateOne({ _id: id }, { $set: update });
-    res.send({msg : "User updated successfully!"});
+    res.send({ msg: "User updated successfully!" });
   } catch (e) {
     res.status(400).send({ msg: e });
   }
 });
-
-
-
 
 module.exports = userRouter;
