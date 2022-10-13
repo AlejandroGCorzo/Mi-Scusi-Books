d312ce1bf36c7e9c3fac5b26cd12eefae7a911c3
JanoNanzer <janonanzer@gmail.com>
2022-10-13 16:17:47 -0300

    FRONT App merge createForm and userDetails

--- client/src/components/UserDetails/UserDetails.jsx
+++ client/src/components/UserDetails/UserDetails.jsx
@@ -1,35 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
-import './UserDetails.css'
-// import { getDetail, getBooks } from "../../redux/StoreBooks/booksActions.js";
-// import { setEmptyDetail } from "../../redux/StoreBooks/booksSlice.js";
+import "./UserDetails.css";
 
 const UserDetails = () => {
   const dispatch = useDispatch();
 
   const { loggedUser } = useSelector((state) => state.users);
 
-//   useEffect(() => {
-//     dispatch(getBooks());
-//     dispatch(getDetail(props.match.params.id));
-//     return () => {
-//       dispatch(setEmptyDetail());
-//     };
-//   }, [dispatch]);
-
   return (
     <div className="userOuterDiv">
-        <div className="userInnerDiv">
+      <div className="userInnerDiv">
         <h1>Profile ✎</h1>
-        <img src={loggedUser.picture} className='userImage'/>
+        <img src={loggedUser.picture} className="userImage" />
         <span>Username: {loggedUser.nickname}</span>
         <span>First Name: {loggedUser.firstName}</span>
         <span>Last Name: {loggedUser.lastName}</span>
         <span>E-mail: {loggedUser.email}</span>
         <span>DNI: {loggedUser.dni}</span>
         <span>Phone: {loggedUser.phone}</span>
-        
+
         <span>Address: </span>
         <span>Street: {loggedUser.address.street}</span>
         <span>Number: {loggedUser.address.number}</span>
@@ -38,7 +28,7 @@ const UserDetails = () => {
         <span>Birthday: {loggedUser.birthdate}</span>
         <span>Verify: {loggedUser.state}</span>
         <span>Bills: {loggedUser.bills}</span>
-        </div>
+      </div>
     </div>
   );
 };
