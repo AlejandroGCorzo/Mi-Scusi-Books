a4bf8eadd6cd4fbd34391a5f7fcf200275f9614e
AlejandroGCorzo <alejandro.g.corzo@gmail.com>
2022-10-14 19:56:11 -0300

    modularización finalizada

---
+++ client/src/components/CreateBook/SubmitButtonsStack/SubmitButtonStack.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+export default function SubmitButtonStack({
+  handleClickOpen,
+  errorHandler,
+  newBook,
+}) {
+  return (
+    <Stack className="CreateBookConfirmationStack" direction="row" spacing={2}>
+      <Button
+        name="reset"
+        variant="outlined"
+        startIcon={<DeleteIcon />}
+        onClick={handleClickOpen}
+      >
+        Reset fields
+      </Button>
+      <Button
+        name="create"
+        variant="contained"
+        endIcon={<SendIcon />}
+        onClick={handleClickOpen}
+        disabled={
+          errorHandler.edition.length !== 0 ||
+          errorHandler.price.length !== 0 ||
+          errorHandler.synopsis.length !== 0 ||
+          errorHandler.ISBN.length !== 0 ||
+          errorHandler.stock.length !== 0 ||
+          !newBook.title ||
+          !newBook.title ||
+          !newBook.author.length ||
+          !newBook.editorial ||
+          !newBook.edition.length ||
+          !newBook.price ||
+          newBook.categories.length < 2 ||
+          !newBook.synopsis ||
+          !newBook.format ||
+          !newBook.language ||
+          !newBook.ISBN ||
+          !newBook.stock ||
+          !newBook.image
+        }
+      >
+        Create Book
+      </Button>
+    </Stack>
+  );
+}
