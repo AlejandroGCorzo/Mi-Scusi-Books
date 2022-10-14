fb6842bf0838222624b288c25abef77be499d040
kripto-c <mgutierrezxred@gmail.com>
2022-10-14 17:56:54 -0400

    modifict filter type and value

--- api/src/routes/books.js
+++ api/src/routes/books.js
@@ -53,7 +53,7 @@ bookRouter.post('/', async (req, res) => {
       editorial &&
       price &&
       category &&
-      synopsis.length > 30 &&
+      synopsis &&
       format &&
       edition &&
       language &&
@@ -112,7 +112,8 @@ bookRouter.get('/', async (req, res) => {
 // valid filter type Two author || category || rating || reviews
 bookRouter.get("/filter", async (req, res) => {
   const { type, value } = req.query;
-  let filtro = [type.toLowerCase(), value.split("%20").join(" ")];
+  // let filtro = [type.toLowerCase(), value.split("%20").join(" ").toLowerCase()];
+  let filtro = [type.split("-").join(" ").toLowerCase(), value.split("-").join(" ")]
 
   try {
     if (filterTypeOne.includes(type)) {
