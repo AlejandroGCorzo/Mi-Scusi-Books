7998b629a27dd1b4f1a5c129ae2446c2f8346633
AlejandroGCorzo <alejandro.g.corzo@gmail.com>
2022-10-14 19:20:55 -0300

    modularización parcial

--- client/src/components/CreateBook/CreateBook.jsx
+++ client/src/components/CreateBook/CreateBook.jsx
@@ -10,11 +10,8 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import SendIcon from '@mui/icons-material/Send';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import DialogAction from './DialogActions/DialogActions.jsx';
+
 import {
   deleteCategory,
   handleErrors,
@@ -275,62 +272,19 @@ export default function CreateBook() {
           </Button>
         </Stack>
         {/* CONFIRM RESET DIALOG */}
-        <Dialog
-          open={open.reset ? true : false}
-          onClose={handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle id="alert-dialog-title">Confirm Reset</DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              Are you sure you want to reset all fields?
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button
-              onClick={() => {
-                handleClose();
-                setNewBook(emptyBook);
-                setAuthor('');
-                setOptions(defaultOptions);
-                setCatSel('Select theme');
-              }}
-              autoFocus
-            >
-              Confirm
-            </Button>
-            <Button onClick={handleClose}>Cancel</Button>
-          </DialogActions>
-        </Dialog>
-        {/* CONFIRM CREATE DIALOG */}
-        <Dialog
-          open={open.create ? true : false}
-          onClose={handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle id="alert-dialog-title">
-            Confirm creation of {newBook.title}?
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              Check that everything is OK and click Confirm. Else click Cancel.
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button
-              onClick={(e) => {
-                handleClose();
-                handleSubmit(e, newBook, history);
-              }}
-              autoFocus
-            >
-              Confirm
-            </Button>
-            <Button onClick={handleClose}>Cancel</Button>
-          </DialogActions>
-        </Dialog>
+        <DialogAction
+          handleClose={handleClose}
+          newBook={newBook}
+          setNewBook={setNewBook}
+          emptyBook={emptyBook}
+          handleSubmit={handleSubmit}
+          setAuthor={setAuthor}
+          setOptions={setOptions}
+          defaultOptions={defaultOptions}
+          setCatSel={setCatSel}
+          open={open}
+          history={history}
+        />
         {/*  */}
       </div>
       {/*  */}
