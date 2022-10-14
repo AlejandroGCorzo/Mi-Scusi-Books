cee6cb9bbc0f7378480537ba8024de8ba3e5272f
JanoNanzer <janonanzer@gmail.com>
2022-10-14 16:30:15 -0300

    FRONT searBar use route back

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -3,7 +3,10 @@ import { useDispatch, useSelector } from "react-redux";
 import "./SearchBar.css";
 import Search from "../../sourceImg/search.svg";
 import { getBookByName } from "../../redux/StoreBooks/booksSlice";
+import { getBookName } from "../../redux/StoreBooks/booksActions";
 import { useHistory } from "react-router-dom";
+
+// import { setEmptyDetail } from "../../redux/StoreBooks/booksSlice.js";
 
 export default function SearchBar() {
   const [name, setName] = useState("");
@@ -13,14 +16,17 @@ export default function SearchBar() {
   const { bookByName } = useSelector((state) => state.books);
   const { books } = useSelector((state) => state.books);
 
+  // const { detail } = useSelector((state) => state.books);
+
   function handleInputChange(i) {
     setName(i.target.value);
   }
 
-  function handleSubmit(i) {
+  async function handleSubmit(i) {
     i.preventDefault();
-    dispatch(getBookByName(name));
-    history.push(`/book_details/${books.find(b=>b.name === name)._id}`);
+    // dispatch(setEmptyDetail());
+    dispatch(getBookName("name", name));
+    history.push(`/book_details/${books.find((b) => b.name === name)._id}`);
     setName("");
   }
 
