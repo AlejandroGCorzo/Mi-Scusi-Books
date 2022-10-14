332513707aac8d43d17ea99cb3543d4cd9e20395
AlejandroGCorzo <alejandro.g.corzo@gmail.com>
2022-10-14 18:48:40 -0300

    merge

--- client/src/components/CreateBook/CreateBook.jsx
+++ client/src/components/CreateBook/CreateBook.jsx
@@ -80,252 +80,258 @@ export default function CreateBook() {
   return (
     <div className="bookCreationForm">
       <div className="bookCreationFormInput">
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <div>
-            <span>Title: </span>
-            <input
-              type="text"
-              placeholder="Write here"
-              name="title"
-              value={newBook.title}
-              onChange={handleChange}
-            />
-          </div>
-          <span></span>
-          <div>
-            <span>Author: </span>
-            <input
-              type="text"
-              placeholder="Write here"
-              name="author"
-              value={author}
-              onChange={(e) => {
-                setAuthor(e.target.value);
-              }}
-              onKeyDown={(e) => {
-                if (e.keyCode === 13 && e.target.name === 'author')
-                  return onChange(e, newBook, setNewBook, author, setAuthor);
-              }}
-            />
-            <button
-              name="author"
-              disabled={!author.length}
-              onClick={handleChange}
-            >
-              add
-            </button>
-          </div>
-          <span></span>
-          <div>
-            <span>Editorial: </span>
-            <input
-              type="text"
-              placeholder="Write here"
-              name="editorial"
-              value={newBook.editorial}
-              onChange={handleChange}
-            />
-          </div>
-          <span></span>
-          <div>
-            <span>Edition: </span>
-            <input
-              type="text"
-              placeholder="Year edition"
-              name="edition"
-              value={newBook.edition}
-              onChange={handleChange}
-            />
-          </div>
-          <span>{errorHandler.edition}</span>
-          <div>
-            <span>Price: </span>
-            <input
-              type="text"
-              placeholder="Numbers only"
-              name="price"
-              value={newBook.price}
-              onChange={handleChange}
-            />
-          </div>
-          <span>{errorHandler.price}</span>
-          <CategoriesSelector
+        <div>
+          <span>Title: </span>
+          <input
+            type="text"
+            placeholder="Write here"
+            name="title"
+            value={newBook.title}
+            onChange={handleChange}
+          />
+        </div>
+        <span></span>
+        <div>
+          <span>Author: </span>
+          <input
+            type="text"
+            placeholder="Write here"
+            name="author"
+            value={author}
+            onChange={(e) => {
+              setAuthor(e.target.value);
+            }}
+            onKeyDown={(e) => {
+              if (e.keyCode === 13 && e.target.name === 'author')
+                return onChange(e, newBook, setNewBook, author, setAuthor);
+            }}
+          />
+          <button
+            name="author"
+            disabled={!author.length}
+            onClick={handleChange}
+          >
+            add
+          </button>
+        </div>
+        <span></span>
+        <div>
+          <span>Editorial: </span>
+          <input
+            type="text"
+            placeholder="Write here"
+            name="editorial"
+            value={newBook.editorial}
+            onChange={handleChange}
+          />
+        </div>
+        <span></span>
+        <div>
+          <span>Edition: </span>
+          <input
+            type="text"
+            placeholder="Year edition"
+            name="edition"
+            value={newBook.edition}
+            onChange={handleChange}
+          />
+        </div>
+        <span>{errorHandler.edition}</span>
+        <div>
+          <span>Price: </span>
+          <input
+            type="text"
+            placeholder="Numbers only"
+            name="price"
+            value={newBook.price}
+            onChange={handleChange}
+          />
+        </div>
+        <span>{errorHandler.price}</span>
+        <CategoriesSelector
+          newBook={newBook}
+          setNewBook={setNewBook}
+          catSel={catSel}
+          setCatSel={setCatSel}
+          categories={categories}
+        />
+        <div>
+          <span>Synopsis: </span>
+          <TextareaAutosize
+            aria-label="minimum height"
+            minRows={3}
+            placeholder="Write here"
+            style={{ width: 200 }}
+            name="synopsis"
+            value={newBook.synopsis}
+            onChange={handleChange}
+          />
+        </div>
+        <span>{errorHandler.synopsis}</span>
+        <div>
+          <span>Format: </span>
+          <select
+            value={options.format}
+            onChange={(e) => {
+              setNewBook({ ...newBook, format: e.target.value });
+              setOptions({ ...options, format: e.target.value });
+            }}
+          >
+            <option disabled>Select format</option>
+            <option>Digital</option>
+            <option>Hardcover</option>
+            <option>Paperback</option>
+          </select>
+        </div>
+        <div>
+          <span>Language: </span>
+          <select
+            value={options.language}
+            onChange={(e) => {
+              setNewBook({ ...newBook, language: e.target.value });
+              setOptions({ ...options, language: e.target.value });
+            }}
+          >
+            <option disabled>Select language</option>
+            <option>English</option>
+            <option>Spanish</option>
+          </select>
+        </div>
+        <div>
+          <span>ISBN: </span>
+          <input
+            type="text"
+            placeholder="Numbers only"
+            name="ISBN"
+            value={newBook.ISBN}
+            onChange={handleChange}
+            maxLength={13}
+          />
+        </div>
+        <span>{errorHandler.ISBN}</span>
+        <div>
+          <span>Stock: </span>
+          <input
+            type="text"
+            placeholder="Numbers only"
+            name="stock"
+            value={newBook.stock}
+            onChange={handleChange}
+          />
+        </div>
+        <span>{errorHandler.stock}</span>
+        <div>
+          <ImgSelector
+            imgSelected={imgSelected}
+            setImgSelected={setImgSelected}
             newBook={newBook}
             setNewBook={setNewBook}
             catSel={catSel}
             setCatSel={setCatSel}
             categories={categories}
           />
-          <div>
-            <span>Synopsis: </span>
-            <TextareaAutosize
-              aria-label="minimum height"
-              minRows={3}
-              placeholder="Write here"
-              style={{ width: 200 }}
-              name="synopsis"
-              value={newBook.synopsis}
-              onChange={handleChange}
-            />
-          </div>
-          <span>{errorHandler.synopsis}</span>
-          <div>
-            <span>Format: </span>
-            <select
-              value={options.format}
-              onChange={(e) => {
-                setNewBook({ ...newBook, format: e.target.value });
-                setOptions({ ...options, format: e.target.value });
-              }}
-            >
-              <option disabled>Select format</option>
-              <option>Digital</option>
-              <option>Hardcover</option>
-              <option>Paperback</option>
-            </select>
-          </div>
-          <div>
-            <span>Language: </span>
-            <select
-              value={options.language}
-              onChange={(e) => {
-                setNewBook({ ...newBook, language: e.target.value });
-                setOptions({ ...options, language: e.target.value });
-              }}
-            >
-              <option disabled>Select language</option>
-              <option>English</option>
-              <option>Spanish</option>
-            </select>
-          </div>
-          <div>
-            <span>ISBN: </span>
-            <input
-              type="text"
-              placeholder="Numbers only"
-              name="ISBN"
-              value={newBook.ISBN}
-              onChange={handleChange}
-              maxLength={13}
-            />
-          </div>
-          <span>{errorHandler.ISBN}</span>
-          <div>
-            <span>Stock: </span>
-            <input
-              type="text"
-              placeholder="Numbers only"
-              name="stock"
-              value={newBook.stock}
-              onChange={handleChange}
-            />
-          </div>
-          <span>{errorHandler.stock}</span>
-          <div>
-            <ImgSelector
-              imgSelected={imgSelected}
-              setImgSelected={setImgSelected}
-              newBook={newBook}
-              setNewBook={setNewBook}
-            />
-          </div>
-          {/* STACK BOTONES RESET Y CREATE */}
-          <Stack
-            className="CreateBookConfirmationStack"
-            direction="row"
-            spacing={2}
+        </div>
+        {/* STACK BOTONES RESET Y CREATE */}
+        <Stack
+          className="CreateBookConfirmationStack"
+          direction="row"
+          spacing={2}
+        >
+          <Button
+            name="reset"
+            variant="outlined"
+            startIcon={<DeleteIcon />}
+            onClick={handleClickOpen}
           >
+            Reset fields
+          </Button>
+          <Button
+            name="create"
+            variant="contained"
+            endIcon={<SendIcon />}
+            onClick={handleClickOpen}
+            disabled={
+              errorHandler.edition.length !== 0 ||
+              errorHandler.price.length !== 0 ||
+              errorHandler.synopsis.length !== 0 ||
+              errorHandler.ISBN.length !== 0 ||
+              errorHandler.stock.length !== 0 ||
+              !newBook.title ||
+              !newBook.title ||
+              !newBook.author.length ||
+              !newBook.editorial ||
+              !newBook.edition.length ||
+              !newBook.price ||
+              newBook.categories.length < 2 ||
+              !newBook.synopsis ||
+              !newBook.format ||
+              !newBook.language ||
+              !newBook.ISBN ||
+              !newBook.stock ||
+              !newBook.image
+            }
+          >
+            Create Book
+          </Button>
+        </Stack>
+        {/* CONFIRM RESET DIALOG */}
+        <Dialog
+          open={open.reset ? true : false}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">Confirm Reset</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Are you sure you want to reset all fields?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
             <Button
-              name="reset"
-              variant="outlined"
-              startIcon={<DeleteIcon />}
-              onClick={handleClickOpen}
+              onClick={() => {
+                handleClose();
+                setNewBook(emptyBook);
+                setAuthor('');
+                setOptions(defaultOptions);
+                setCatSel('Select theme');
+              }}
+              autoFocus
             >
-              Reset fields
+              Confirm
             </Button>
+            <Button onClick={handleClose}>Cancel</Button>
+          </DialogActions>
+        </Dialog>
+        {/* CONFIRM CREATE DIALOG */}
+        <Dialog
+          open={open.create ? true : false}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            Confirm creation of {newBook.title}?
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Check that everything is OK and click Confirm. Else click Cancel.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
             <Button
-              name="create"
-              variant="contained"
-              endIcon={<SendIcon />}
-              onClick={handleClickOpen}
-              disabled={
-                errorHandler.edition ||
-                errorHandler.price ||
-                errorHandler.synopsis ||
-                errorHandler.ISBN ||
-                errorHandler.stock ||
-                !newBook.title ||
-                !newBook.title ||
-                !newBook.author.length ||
-                !newBook.editorial ||
-                !newBook.edition ||
-                !newBook.price ||
-                newBook.categories.length < 2 ||
-                !newBook.synopsis ||
-                !newBook.format ||
-                !newBook.language ||
-                !newBook.ISBN ||
-                !newBook.stock ||
-                !newBook.image
-              }
+              onClick={(e) => {
+                handleClose();
+                handleSubmit(e, newBook, history);
+              }}
+              autoFocus
             >
-              Create Book
+              Confirm
             </Button>
-          </Stack>
-          {/* CONFIRM RESET DIALOG */}
-          <Dialog
-            open={open.reset ? true : false}
-            onClose={handleClose}
-            aria-labelledby="alert-dialog-title"
-            aria-describedby="alert-dialog-description"
-          >
-            <DialogTitle id="alert-dialog-title">Confirm Reset</DialogTitle>
-            <DialogContent>
-              <DialogContentText id="alert-dialog-description">
-                Are you sure you want to reset all fields?
-              </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-              <Button
-                onClick={() => {
-                  handleClose();
-                  setNewBook(emptyBook);
-                  setAuthor('');
-                  setOptions(defaultOptions);
-                  setCatSel('Select theme');
-                }}
-                autoFocus
-              >
-                Confirm
-              </Button>
-              <Button onClick={handleClose}>Cancel</Button>
-            </DialogActions>
-          </Dialog>
-          {/* CONFIRM CREATE DIALOG */}
-          <Dialog
-            open={open.create ? true : false}
-            onClose={handleClose}
-            aria-labelledby="alert-dialog-title"
-            aria-describedby="alert-dialog-description"
-          >
-            <DialogTitle id="alert-dialog-title">
-              Confirm creation of {newBook.title}?
-            </DialogTitle>
-            <DialogContent>
-              <DialogContentText id="alert-dialog-description">
-                Check that everything is OK and click Confirm. Else click
-                Cancel.
-              </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleClose} autoFocus>
-                Confirm
-              </Button>
-              <Button onClick={handleClose}>Cancel</Button>
-            </DialogActions>
-          </Dialog>
-          {/*  */}
-        </form>
+            <Button onClick={handleClose}>Cancel</Button>
+          </DialogActions>
+        </Dialog>
+        {/*  */}
       </div>
       {/*  */}
       <div>
