838d704991f83f0fe9f18508e301b17bda70b92a
AlejandroGCorzo <alejandro.g.corzo@gmail.com>
2022-10-14 11:25:26 -0300

    modularizada la función handle errors

--- client/src/components/CreateBook/CreateBook.jsx
+++ client/src/components/CreateBook/CreateBook.jsx
@@ -7,6 +7,7 @@ import ImgSelector from './ImgSelector/ImgSelector.jsx';
 import NewBookPreview from './NewBookPreview/NewBookPreview.jsx';
 import TextareaAutosize from '@mui/material/TextareaAutosize';
 import './CreateBook.css';
+import { handleErrors } from './Functions/handleErrors';
 
 export default function CreateBook() {
   const [newBook, setNewBook] = useState({
@@ -66,80 +67,10 @@ export default function CreateBook() {
     if (newBook.categories.length === 2) setCatSel('Select category');
     if (newBook.categories.length === 3) setCatSel('Select subcategory');
   }
-  // // // // // //
-  function handleErrors(e) {
-    const onlyNumber = new RegExp(/^[0-9]*$/);
-    const allowDecimal = new RegExp(/^\d*\.{0,1}\d{0,2}$/);
-    if (e.target.name === 'stock') {
-      if (!onlyNumber.test(e.target.value)) {
-        setErrorHandler({
-          ...errorHandler,
-          [e.target.name]: 'Only numbers allowed',
-        });
-      } else {
-        setErrorHandler({ ...errorHandler, [e.target.name]: '' });
-      }
-    }
-    if (e.target.name === 'ISBN') {
-      if (!onlyNumber.test(e.target.value)) {
-        setErrorHandler({
-          ...errorHandler,
-          [e.target.name]: 'Only numbers allowed',
-        });
-      } else if (e.target.name.length !== 13) {
-        setErrorHandler({
-          ...errorHandler,
-          [e.target.name]: 'Must contain 13 numbers',
-        });
-      } else setErrorHandler({ ...errorHandler, [e.target.name]: '' });
-    }
-
-    if (e.target.name === 'edition') {
-      if (!onlyNumber.test(e.target.value)) {
-        setErrorHandler({
-          ...errorHandler,
-          [e.target.name]: 'Only numbers allowed',
-        });
-      } else if (
-        e.target.value.length !== 4 ||
-        e.target.value > Number(new Date().getFullYear()) ||
-        e.target.value < 1800
-      ) {
-        setErrorHandler({
-          ...errorHandler,
-          [e.target.name]: 'Must be a valid Year.',
-        });
-      } else {
-        setErrorHandler({ ...errorHandler, [e.target.name]: '' });
-      }
-    }
-
-    if (e.target.name === 'price')
-      if (!allowDecimal.test(e.target.value)) {
-        setErrorHandler({
-          ...errorHandler,
-          [e.target.name]:
-            'Only numbers and one dot followed by two decimals allowed',
-        });
-      } else {
-        setErrorHandler({ ...errorHandler, [e.target.name]: '' });
-      }
-
-    if (e.target.name === 'synopsis') {
-      if (e.target.value.length > 300) {
-        setErrorHandler({
-          ...errorHandler,
-          [e.target.name]: '300 characters allowed',
-        });
-      } else {
-        setErrorHandler({ ...errorHandler, [e.target.name]: '' });
-      }
-    }
-  }
 
   function handleChange(e) {
     onChange(e);
-    handleErrors(e);
+    handleErrors(e, errorHandler, setErrorHandler);
   }
   // // // // // //
   useEffect(() => {
