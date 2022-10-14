c574acc689844c40a58065782dd91e7b65c40914
kripto-c <mgutierrezxred@gmail.com>
2022-10-14 17:28:47 -0400

    route books/filter modifict type and value

--- api/src/routes/books.js
+++ api/src/routes/books.js
@@ -35,7 +35,7 @@ bookRouter.post('/', async (req, res) => {
     } = req.body;
     let theme = await Category.find();
     const id = theme[0]._id;
-    // console.log(id);
+    // // console.log(id);
 
     //Check if it has 3 cats
     if (categories.length === 3) {
@@ -54,7 +54,7 @@ bookRouter.post('/', async (req, res) => {
       editorial &&
       price &&
       category &&
-      synopsis.length > 30 &&
+      synopsis  &&
       format &&
       edition &&
       language &&
@@ -114,7 +114,7 @@ bookRouter.get('/', async (req, res) => {
 
 bookRouter.get("/filter", async (req, res) => {
   const { type, value } = req.query;
-  let filtro = [type.split("-").join(" ").toLowerCase(), value.split("-").join(" ")]
+  let filtro = [type.toLowerCase().split("-").join(" "), value.split("-").join(" ").toLowerCase()];
 
   try {
     if (filterTypeOne.includes(type)) {
@@ -267,8 +267,11 @@ bookRouter.put("/:id", async (req, res, next) => {
 
 //midleware error handling
 bookRouter.use((error, req, res, next) => {
-  if (error.name === "CastError") res.status(400).send({ error: "the data sent is malformed " });
-  else if (error.name === "TypeError") res.status(400).json({error: error + ""})
+  if (error.name === "CastError") {
+    res.status(400).send({ error: "the data sent is malformed " });
+  }else if (error.name === "TypeError"){
+      res.status(400).json({error: error + ""})
+  } 
   else res.status(500).end()
 });
 
