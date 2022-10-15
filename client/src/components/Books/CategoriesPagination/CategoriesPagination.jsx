e254b7667bc375cab939a078c2ac6d7bb4594177
AlejandroGCorzo <alejandro.g.corzo@gmail.com>
2022-10-15 02:46:41 -0300

    mas cambios

--- client/src/components/Books/CategoriesPagination/CategoriesPagination.jsx
+++ client/src/components/Books/CategoriesPagination/CategoriesPagination.jsx
@@ -1,53 +1,70 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import Typography from '@mui/material/Typography';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
 
 export default function CategoriesPagination({ theme, category, subcategory }) {
+  function handleClick(event) {
+    event.preventDefault();
+    console.info('You clicked a breadcrumb.');
+  }
   return (
-    <div style={{ display: 'flex' }}>
-      <Link to={'/books'} style={{ textDecoration: 'none' }}>
-        Books
-      </Link>
-      {theme ? (
-        category ? (
-          subcategory ? (
-            <React.Fragment>
-              {' --> '}
-              <Link to={`/books/${theme}`} style={{ textDecoration: 'none' }}>
-                {theme}
-              </Link>
-              {' --> '}
-              <Link
-                to={`/books/${theme}/${category}`}
-                style={{ textDecoration: 'none' }}
-              >
-                {category}
-              </Link>
-              {` --> ${subcategory}`}
-            </React.Fragment>
-          ) : (
-            <React.Fragment>
-              {' --> '}
-              <Link to={`/books/${theme}`} style={{ textDecoration: 'none' }}>
-                {theme}
-              </Link>
-              {' --> '}
-              <Link
-                to={`/books/${theme}/${category}`}
-                style={{ textDecoration: 'none' }}
-              >
-                {category}
-              </Link>
-            </React.Fragment>
-          )
-        ) : (
-          <React.Fragment>
-            {' --> '}
-            <Link to={`/books/${theme}`} style={{ textDecoration: 'none' }}>
-              {theme}
-            </Link>
-          </React.Fragment>
-        )
-      ) : null}
+    <div role="presentation" onClick={handleClick}>
+      <Breadcrumbs aria-label="breadcrumb">
+        <Link underline="hover" color="inherit" href="/">
+          MUI
+        </Link>
+        <Link
+          underline="hover"
+          color="inherit"
+          href="/material-ui/getting-started/installation/"
+        >
+          Core
+        </Link>
+        <Typography color="text.primary">Breadcrumbs</Typography>
+      </Breadcrumbs>
     </div>
+    // <div style={{ display: 'flex' }}>
+    //   <Link to={'/books'} style={{ textDecoration: 'none' }}>
+    //     Books
+    //   </Link>
+    //   {theme ? (
+    //     category ? (
+    //       subcategory ? (
+    //         <React.Fragment>
+    //           <Link to={`/books/${theme}`} style={{ textDecoration: 'none' }}>
+    //             {`/  ${theme}`}
+    //           </Link>{' '}
+    //           <Link
+    //             to={`/books/${theme}/${category}`}
+    //             style={{ textDecoration: 'none' }}
+    //           >
+    //             {`/ ${category}`}
+    //           </Link>
+    //           {`/ ${subcategory}`}
+    //         </React.Fragment>
+    //       ) : (
+    //         <React.Fragment>
+    //           <Link to={`/books/${theme}`} style={{ textDecoration: 'none' }}>
+    //             {theme}
+    //           </Link>
+    //           <Link
+    //             to={`/books/${theme}/${category}`}
+    //             style={{ textDecoration: 'none' }}
+    //           >
+    //             {category}
+    //           </Link>
+    //         </React.Fragment>
+    //       )
+    //     ) : (
+    //       <React.Fragment>
+    //         <Link to={`/books/${theme}`} style={{ textDecoration: 'none' }}>
+    //           {theme}
+    //         </Link>
+    //       </React.Fragment>
+    //     )
+    //   ) : null}
+    // </div>
   );
 }
