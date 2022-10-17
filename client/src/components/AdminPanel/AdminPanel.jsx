75d888c3acafd4f09758b664ff2ae3dc202c7409
JanoNanzer <janonanzer@gmail.com>
2022-10-17 14:37:09 -0300

    FRONT AdminPanel start

---
+++ client/src/components/AdminPanel/AdminPanel.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import UsersTable from './UsersTable/UsersTable.jsx'
+
+import { useDispatch, useSelector } from "react-redux";
+import { getUser } from "../../redux/StoreUsers/usersActions.js";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+export default function BasicTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUser());
+  }, [dispatch]);
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          <Tab label="Users" {...a11yProps(0)} />
+          <Tab label="Books" {...a11yProps(1)} />
+          <Tab label="Payments" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        <UsersTable/>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <div>
+          <h1>TAB 2</h1>
+        </div>
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        TAB 3
+      </TabPanel>
+    </Box>
+  );
+}
---
+++ client/src/components/AdminPanel/UsersTable/UsersTable.jsx
@@ -0,0 +1,72 @@
+import * as React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+// function createData(name, calories, fat, carbs, protein) {
+//   return { name, calories, fat, carbs, protein };
+// }
+
+// const rows = [
+//   createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+//   createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+//   createData("Eclair", 262, 16.0, 24, 6.0),
+//   createData("Cupcake", 305, 3.7, 67, 4.3),
+//   createData("Gingerbread", 356, 16.0, 49, 3.9),
+// ];
+
+export default function DenseTable() {
+
+    const { users } = useSelector((state) => state.users);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Email</TableCell>
+            <TableCell align="right">First Name</TableCell>
+            <TableCell align="right">Last Name</TableCell>
+            <TableCell align="right">DNI</TableCell>
+            <TableCell align="right">Phone</TableCell>
+            <TableCell align="right">Birthday</TableCell>
+
+            <TableCell align="right">Street</TableCell>
+            <TableCell align="right">Number</TableCell>
+            <TableCell align="right">Floor</TableCell>
+
+            <TableCell align="right">State</TableCell>
+            <TableCell align="right">Type</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users.map((u) => (
+            <TableRow
+              key={u._id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {u.email}
+              </TableCell>
+              <TableCell align="right">{u.firstName}</TableCell>
+              <TableCell align="right">{u.lastName}</TableCell>
+              <TableCell align="right">{u.dni}</TableCell>
+              <TableCell align="right">{u.phone}</TableCell>
+              <TableCell align="right">{u.birthdate}</TableCell>
+              <TableCell align="right">{u.address.street}</TableCell>
+              <TableCell align="right">{u.address.number}</TableCell>
+              <TableCell align="right">{u.address.floor}</TableCell>
+              <TableCell align="right">{u.state}</TableCell>
+              <TableCell align="right">{u.type}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
