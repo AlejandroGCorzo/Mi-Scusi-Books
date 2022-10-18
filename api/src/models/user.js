88ba9845a37f0d6ccdd62d3bf55bfddec66cb1d0
Juanfrancoledesma <juanfledesma18@gmail.com>
2022-10-18 09:56:17 -0300

    routes user

--- api/src/models/user.js
+++ api/src/models/user.js
@@ -61,6 +61,21 @@ const userSchema = mongoose.Schema(
       type: [mongoose.Schema.Types.ObjectId],
       ref: "Bills"
     },
+    votedBooks:{
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "Books"
+    },
+    favorites:{
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "Books"
+    },
+    cart:{
+      books:{
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: "Books"
+      },
+      amounts: [Number]
+    }
   }, { timestamps: false }
 );
 
