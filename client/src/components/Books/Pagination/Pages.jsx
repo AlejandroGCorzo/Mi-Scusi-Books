319bf00e5a38d172cf8a2ad3d0258fd03bdda8f1
juan ricaud <ricaudjuan@hotmail.com>
2022-10-17 23:29:35 -0300

    new pagination and testing protect

--- client/src/components/Books/Pagination/Pages.jsx
+++ client/src/components/Books/Pagination/Pages.jsx
@@ -1,5 +1,6 @@
-import { Box, Pagination } from "@mui/material";
-import { useEffect, useState } from "react";
+import { Box, Pagination, TablePagination } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { setCurrentPage } from "../../../redux/StoreBooks/booksActions";
 
 
 export default function Pages({ count, setBooksToShow, setPagination }) {
@@ -16,17 +17,31 @@ export default function Pages({count}){
  const { page } = useSelector(
   (state) => state.books
 );
-  function handleChange(e, page, selected){
-    console.log('aca', page, selected)
-    dispatch(setCurrentPage(page))
+  // function handleChange(e, page, selected){
+  //   console.log('aca', page, selected)
+  //   dispatch(setCurrentPage(page))
+  // }
+  function onRowsChange(e){
+    const rows = parseInt(e.target.value, 10)
+    dispatch(setCurrentPage({...page, rows: rows}))
   }
 
-  return (
+  function onPageChange(e, newPage){
+    dispatch(setCurrentPage({...page, currentPage: newPage}))
+  }
+  return(
     <Box>
-      <Pagination 
+      {/* <Pagination 
       count={count}
       onChange={handleChange}
       page={page}
+      /> */}
+      <TablePagination
+        count={count}
+        page={page.currentPage}
+        onPageChange={onPageChange}
+        rowsPerPage={page.rows}
+        onRowsPerPageChange={onRowsChange}
       />
     </Box>
   );
