d73861d2f404754b52316c420ae0ae1aecaf5ae6
AlejandroGCorzo <alejandro.g.corzo@gmail.com>
2022-10-17 21:33:48 -0300

    pritiada userDetails.jsx

--- client/src/components/UserDetails/UserDetails.jsx
+++ client/src/components/UserDetails/UserDetails.jsx
@@ -10,62 +10,61 @@ const UserDetails = () => {
 
   return (
     <div className="userOuterDiv">
-        <div className="contentCategoryx">
-            <div className="titleFormx">
-                <p>Profile</p>
-                <button className="pencilButtonx">✎</button>
+      <div className="contentCategoryx">
+        <div className="titleFormx">
+          <p>Profile</p>
+          <button className="pencilButtonx">✎</button>
+        </div>
+
+        <div className="contentCategoryDivx">
+          <div className="categoryDivx">
+            <div className="userImage">
+              <img src={loggedUser.picture} />
             </div>
 
-            <div className="contentCategoryDivx">
-                <div className="categoryDivx">
-
-                  <div className="userImage">
-                    <img src={loggedUser.picture} />
-                  </div>
-
-                  <div className="userInfoContainer">
-                    <span>Username: {loggedUser.nickname}</span>
-                    <span>Name: {loggedUser.firstName}</span>
-                    <span>Last Name: {loggedUser.lastName}</span>
-                    <span>E-mail: {loggedUser.email}</span>
-                    <span>DNI: {loggedUser.dni}</span>
-                    <span>Phone: {loggedUser.phone}</span>
-                    <span>Address: {loggedUser.address.street} {loggedUser.address.number}, {loggedUser.address.floor}</span>
-                    {/* <span>Street: {loggedUser.address.street}</span>
+            <div className="userInfoContainer">
+              <span>Username: {loggedUser.nickname}</span>
+              <span>Name: {loggedUser.firstName}</span>
+              <span>Last Name: {loggedUser.lastName}</span>
+              <span>E-mail: {loggedUser.email}</span>
+              <span>DNI: {loggedUser.dni}</span>
+              <span>Phone: {loggedUser.phone}</span>
+              <span>
+                Address: {loggedUser.address.street} {loggedUser.address.number}
+                , {loggedUser.address.floor}
+              </span>
+              {/* <span>Street: {loggedUser.address.street}</span>
                     <span>Number: {loggedUser.address.number}</span>
                     <span>Floor: {loggedUser.address.floor}</span> */}
-                    <span>Birthday: {loggedUser.birthdate}</span>
-                    {/* <span>Verify: {loggedUser.state}</span> */}
-                    <span>Loyalty points: {loggedUser.loyaltyPoint}</span>
-                  </div>
-                </div>
+              <span>Birthday: {loggedUser.birthdate}</span>
+              {/* <span>Verify: {loggedUser.state}</span> */}
+              <span>Loyalty points: {loggedUser.loyaltyPoint}</span>
             </div>
+          </div>
+        </div>
 
-            <div className="formBackx">
-                <Link to ="/" style={{ textDecoration: "none" }}>
-                    <button className="buttonBackx">Save</button>
-                </Link>
-            </div>
+        <div className="formBackx">
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <button className="buttonBackx">Save</button>
+          </Link>
+        </div>
       </div>
 
       <div className="contentCategoryx">
-            <div className="titleFormx">
-                <p>Shopping</p>
-            </div>
-
-            <div className="contentCategoryDivx">
-                <div className="categoryDivx">
-
-                </div>
-            </div>
-
-            <div className="formBackx">
-                <Link to ="/" style={{ textDecoration: "none" }}>
-                    <button className="buttonBackx">View All</button>
-                </Link>
-            </div>
+        <div className="titleFormx">
+          <p>Shopping</p>
         </div>
 
+        <div className="contentCategoryDivx">
+          <div className="categoryDivx"></div>
+        </div>
+
+        <div className="formBackx">
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <button className="buttonBackx">View All</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
