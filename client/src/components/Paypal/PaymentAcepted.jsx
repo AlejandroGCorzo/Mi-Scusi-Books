9842db21ae4b94c33cc0f96076b0124def737031
cassirambd <cassiram15@gmail.com>
2022-10-19 19:55:32 -0300

    component payment css

--- client/src/components/Paypal/PaymentAcepted.jsx
+++ client/src/components/Paypal/PaymentAcepted.jsx
@@ -2,25 +2,30 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import test from "../../sourceImg/book-1.png";
+import "./PaymentAcepted.css"
 
-export default function PaymentAcepted () {
+export default function PaymentAcepted() {
   const dispatch = useDispatch();
   const history = useHistory();
-  //traer al cart 
+  //traer al cart
 
   // useEffect(() {
   //   dispatch(())
   // })
 
   return (
-    <div>
-      <div>
-        <img src={test} alt="" width="300px" />
-      </div>
-      <h1>Payment Succesful!</h1>
-      <h3>Thank you for purchasing</h3>
-      <p>Check your email to view details</p>
-      <button type="button" onClick={() => history.push("/")}>GO HOME</button>
-    </div>
-  )
+    <>
+      <section className="paymentAceptedModal">
+        <div className="divPaymentModal">
+          <img src={test} alt="" className="imgPaymentModal"/>
+        <h1>Payment Succesful!</h1>
+        <h3>Thank you for purchasing</h3>
+        <p>Check your email to view details</p>
+        <button className="btnHome" type="button" onClick={() => history.push("/")}>
+          <b>GO HOME</b>
+        </button>
+        </div>
+      </section>
+    </>
+  );
 }
