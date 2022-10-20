c81ad27056b8e1c789e2c01a5e2b47387d82c9f9
JanoNanzer <janonanzer@gmail.com>
2022-10-20 10:21:54 -0300

    Fix Admin panel(getUsers - DeleteUser)

--- client/src/components/AdminPanel/ConfirmDialog/UsersDelete.jsx
+++ client/src/components/AdminPanel/ConfirmDialog/UsersDelete.jsx
@@ -12,17 +12,21 @@ import { useDispatch } from "react-redux";
 import { setUserDelete } from "../../../redux/StoreUsers/usersActions";
 
 export default function UsersDelete(props) {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const theme = useTheme();
   const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
-  const { numSelected, openDialog, handleClose, id} = props
-
+  const { numSelected, openDialog, handleClose, id } = props;
+  const accessToken =
+    window.localStorage.getItem("token") ||
+    window.sessionStorage.getItem("token");
+    
+  console.log(accessToken);
   console.log(id);
 
-  const handleDelete = (e)=>{
-    dispatch(setUserDelete(id))
-    dispatch(handleClose)
-  }
+  const handleDelete = (e) => {
+    dispatch(setUserDelete(id, "limited", accessToken));
+    dispatch(handleClose);
+  };
 
   return (
     <Dialog
@@ -34,16 +38,16 @@ export default function UsersDelete(props) {
       <DialogTitle id="responsive-dialog-title">{"Delete User"}</DialogTitle>
       <DialogContent>
         <DialogContentText>
-          If you want to delete user {numSelected} please click CONFIRM, otherwise
-          click CANCEL.
+          If you want to delete user {numSelected} please click CONFIRM,
+          otherwise click CANCEL.
         </DialogContentText>
       </DialogContent>
       <DialogActions>
         <Button autoFocus onClick={handleClose}>
-        CANCEL
+          CANCEL
         </Button>
         <Button onClick={handleDelete} autoFocus>
-        CONFIRM
+          CONFIRM
         </Button>
       </DialogActions>
     </Dialog>
