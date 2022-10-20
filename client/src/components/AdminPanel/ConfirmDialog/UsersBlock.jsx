ba525c800db9f1fb2508f9e31c9823a8def07657
JanoNanzer <janonanzer@gmail.com>
2022-10-20 16:24:23 -0300

    FRONT AdminPanel change rol working

--- client/src/components/AdminPanel/ConfirmDialog/UsersBlock.jsx
+++ client/src/components/AdminPanel/ConfirmDialog/UsersBlock.jsx
@@ -11,43 +11,41 @@ import Button from "@mui/material/Button";
 import { useDispatch } from "react-redux";
 import { setUserDelete } from "../../../redux/StoreUsers/usersActions";
 
-export default function UsersDelete(props) {
+
+export default function UsersBlock(props) {
   const dispatch = useDispatch();
   const theme = useTheme();
   const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
-  const { emailSelectUser, openDialog, handleClose, id, setRender } = props;
+  const { emailSelectUser, openBlock, handleCloseBlock, id } = props;
+
   const accessToken =
     window.localStorage.getItem("token") ||
     window.sessionStorage.getItem("token");
 
-  // console.log(accessToken);
-  console.log(id);
-
-  const handleDelete = (e) => {
-    dispatch(setUserDelete(id, "inactive", accessToken));
-    dispatch(handleClose);
-    dispatch(setRender(true))
+  const handleBlock = (e) => {
+    dispatch(setUserDelete(id, "limited", accessToken));
+    dispatch(handleCloseBlock);
   };
 
   return (
     <Dialog
       fullScreen={fullScreen}
-      open={openDialog}
-      onClose={handleClose}
+      open={openBlock}
+      onClose={handleCloseBlock}
       aria-labelledby="responsive-dialog-title"
     >
       <DialogTitle id="responsive-dialog-title">{"Delete User"}</DialogTitle>
       <DialogContent>
         <DialogContentText>
-          If you want to delete user {emailSelectUser} please click CONFIRM,
+          If you want to block user {emailSelectUser} please click CONFIRM,
           otherwise click CANCEL.
         </DialogContentText>
       </DialogContent>
       <DialogActions>
-        <Button autoFocus onClick={(e) => handleDelete(e)}>
+        <Button autoFocus onClick={(e) => handleBlock(e)}>
           CONFIRM
         </Button>
-        <Button onClick={handleClose} autoFocus>
+        <Button onClick={handleCloseBlock} autoFocus>
           CANCEL
         </Button>
       </DialogActions>
