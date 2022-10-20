0573c72ba69e960052b5c089f2d967c415669d03
AlejandroGCorzo <alejandro.g.corzo@gmail.com>
2022-10-20 10:01:03 -0300

    corregido token del detalle de usuario

--- client/src/components/UserDetails/UserDetails.jsx
+++ client/src/components/UserDetails/UserDetails.jsx
@@ -7,85 +7,88 @@ import "./UserDetails.css";
 
 const UserDetails = (props) => {
   const dispatch = useDispatch();
-  const history = useHistory()
+  const history = useHistory();
   const { profile } = useSelector((state) => state.users);
-  const token = window.localStorage.getItem('token');
-  
+  const token =
+    window.localStorage.getItem("token") ||
+    window.sessionStorage.getItem("token");
+
   useEffect(() => {
-      if(!token || profile.msg){
-        history.push("/")
-      } else {
-        dispatch(getUserDetails(props.match.params.id, token))
-      }
-      
-      return () => {
-        dispatch(clearUserDetail())
-      }
-  }, [dispatch])
- 
+    if (!token || profile.msg) {
+      history.push("/");
+    } else {
+      dispatch(getUserDetails(props.match.params.id, token));
+    }
+
+    return () => {
+      dispatch(clearUserDetail());
+    };
+  }, [dispatch]);
+
   return (
     <>
-    { profile._id ? 
-    
-    <div className="userOuterDiv">
-      <div className="contentCategoryx">
-        <div className="titleFormx">
-          <p>Profile</p>
-          <button className="pencilButtonx">✎</button>
-        </div>
-
-        <div className="contentCategoryDivx">
-          <div className="categoryDivx">
-            <div className="userImage">
-              <img src={profile.image} />
+      {profile._id ? (
+        <div className="userOuterDiv">
+          <div className="contentCategoryx">
+            <div className="titleFormx">
+              <p>Profile</p>
+              <button className="pencilButtonx">✎</button>
             </div>
 
-            <div className="userInfoContainer">
-              <span>Username: {profile.username}</span>
-              <span>Name: {profile.firstName}</span>
-              <span>Last Name: {profile.lastName}</span>
-              <span>E-mail: {profile.email}</span>
-              <span>DNI: {profile.dni}</span>
-              <span>Phone: {profile.phone}</span>
-              <span>
-                Address: {profile.address.street} {profile.address.number}
-                , {profile.address.floor}
-              </span>
-              {/* <span>Street: {loggedUser.address.street}</span>
+            <div className="contentCategoryDivx">
+              <div className="categoryDivx">
+                <div className="userImage">
+                  <img src={profile.image} />
+                </div>
+
+                <div className="userInfoContainer">
+                  <span>Username: {profile.username}</span>
+                  <span>Name: {profile.firstName}</span>
+                  <span>Last Name: {profile.lastName}</span>
+                  <span>E-mail: {profile.email}</span>
+                  <span>DNI: {profile.dni}</span>
+                  <span>Phone: {profile.phone}</span>
+                  <span>
+                    Address: {profile.address.street} {profile.address.number},{" "}
+                    {profile.address.floor}
+                  </span>
+                  {/* <span>Street: {loggedUser.address.street}</span>
                     <span>Number: {loggedUser.address.number}</span>
                     <span>Floor: {loggedUser.address.floor}</span> */}
-              <span>Birthday: {profile.birthdate}</span>
-              {/* <span>Verify: {loggedUser.state}</span> */}
-              <span>Loyalty points: {profile.loyaltyPoint}</span>
+                  <span>Birthday: {profile.birthdate}</span>
+                  {/* <span>Verify: {loggedUser.state}</span> */}
+                  <span>Loyalty points: {profile.loyaltyPoint}</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="formBackx">
+              <Link to="/" style={{ textDecoration: "none" }}>
+                <button className="buttonBackx">Save</button>
+              </Link>
+            </div>
+          </div>
+
+          <div className="contentCategoryx">
+            <div className="titleFormx">
+              <p>Shopping</p>
+            </div>
+
+            <div className="contentCategoryDivx">
+              <div className="categoryDivx"></div>
+            </div>
+
+            <div className="formBackx">
+              <Link to="/" style={{ textDecoration: "none" }}>
+                <button className="buttonBackx">View All</button>
+              </Link>
             </div>
           </div>
         </div>
-
-        <div className="formBackx">
-          <Link to="/" style={{ textDecoration: "none" }}>
-            <button className="buttonBackx">Save</button>
-          </Link>
-        </div>
-      </div>
-
-      <div className="contentCategoryx">
-        <div className="titleFormx">
-          <p>Shopping</p>
-        </div>
-
-        <div className="contentCategoryDivx">
-          <div className="categoryDivx"></div>
-        </div>
-
-        <div className="formBackx">
-          <Link to="/" style={{ textDecoration: "none" }}>
-            <button className="buttonBackx">View All</button>
-          </Link>
-        </div>
-      </div>
-    </div>
-     : <></>}
-     </>
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 
