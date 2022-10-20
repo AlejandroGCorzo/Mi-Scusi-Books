71636c79c2735046c62439dfdb758a54bfa87d3c
cassirambd <cassiram15@gmail.com>
2022-10-20 19:17:14 -0300

    acceso a paypal front

--- api/src/routes/paypal.js
+++ api/src/routes/paypal.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const { createOrder, captureOrder, cancelOrder } = require("../paypalcontrollers/PaypalControllers")
 const paypalRouter = Router();
 
-paypalRouter.get("/create-order", createOrder);
+paypalRouter.post("/create-order", createOrder);
 //cambiar a post cuando se le pasen los parametros de el front
 
 paypalRouter.get("/capture-order", captureOrder);
