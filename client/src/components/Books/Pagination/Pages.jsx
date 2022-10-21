6a5961f0e74ad4a8827a7ef6c7fd910597d61133
AlejandroGCorzo <alejandro.g.corzo@gmail.com>
2022-10-20 22:53:21 -0300

    quitado código innecesario de UserLogin

--- client/src/components/Books/Pagination/Pages.jsx
+++ client/src/components/Books/Pagination/Pages.jsx
@@ -1,34 +1,19 @@
-import { Box, Pagination, TablePagination } from "@mui/material";
+import { Box, TablePagination } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { setCurrentPage } from "../../../redux/StoreBooks/booksActions";
 
-
-
-
-export default function Pages({count}){
- const dispatch = useDispatch()
- const { page } = useSelector(
-  (state) => state.books
-);
-  // function handleChange(e, page, selected){
-  //   console.log('aca', page, selected)
-  //   dispatch(setCurrentPage(page))
-  // }
-  function onRowsChange(e){
-    const rows = parseInt(e.target.value, 10)
-    dispatch(setCurrentPage({...page, rows: rows}))
+export default function Pages({ count }) {
+  const dispatch = useDispatch();
+  const { page } = useSelector((state) => state.books);
+  function onRowsChange(e) {
+    const rows = parseInt(e.target.value, 10);
+    dispatch(setCurrentPage({ ...page, rows: rows }));
   }
-
-  function onPageChange(e, newPage){
-    dispatch(setCurrentPage({...page, currentPage: newPage}))
+  function onPageChange(e, newPage) {
+    dispatch(setCurrentPage({ ...page, currentPage: newPage }));
   }
-  return(
+  return (
     <Box>
-      {/* <Pagination 
-      count={count}
-      onChange={handleChange}
-      page={page}
-      /> */}
       <TablePagination
         count={count}
         page={page.currentPage}
