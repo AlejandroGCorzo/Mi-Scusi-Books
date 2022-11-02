f7e6874086a5342daaef6b0589df52b871d08637
cassirambd <cassiram15@gmail.com>
2022-11-02 11:38:05 -0300

    scroll

--- client/src/components/Loader/Loader.jsx
+++ client/src/components/Loader/Loader.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Loader.css";
 
 export default function () {
+  window.scrollTo(0, 0);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="loader">
       <span>Mi Scusi Books...</span>
