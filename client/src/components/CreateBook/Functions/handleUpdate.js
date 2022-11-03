14bd1be603582238b6d7f5bd3ef1ed7ef3b213ad
AlejandroGCorzo <alejandro.g.corzo@gmail.com>
2022-11-03 00:05:43 -0300

    bug fix route book edit

--- client/src/components/CreateBook/Functions/handleUpdate.js
+++ client/src/components/CreateBook/Functions/handleUpdate.js
@@ -1,18 +1,24 @@
-import axios from 'axios'
+import axios from "axios";
 
-export default async function handleUpdate(e, newBook, history, bookId, accessToken){
+export default async function handleUpdate(
+  e,
+  newBook,
+  history,
+  bookId,
+  accessToken
+) {
   e.preventDefault();
-  console.log('token', accessToken)
+  console.log("token", accessToken);
   newBook = { ...newBook, name: newBook.title };
   delete newBook.title;
-  try{
-    const updated = await axios.put(`http://localhost:9000/books/${bookId}`, newBook, {
+  try {
+    const updated = await axios.put(`/books/${bookId}`, newBook, {
       headers: {
         authorization: `Bearer ${accessToken}`,
       },
-    })
-    history.push(`/book_details/${bookId}`)
-  } catch(e){
-    alert('maliosal')
+    });
+    history.push(`/book_details/${bookId}`);
+  } catch (e) {
+    alert("maliosal");
   }
 }
